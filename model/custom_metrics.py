6cc056f5ba58c6b7d52060bba45b42e26866dae4
Antonio Zarauz Moreno <87022369+hedrergudene@users.noreply.github.com>
2021-10-20 11:59:29 +0200

    F1

--- model/custom_metrics.py
+++ model/custom_metrics.py
@@ -2,43 +2,16 @@ import tensorflow as tf
 import keras
 from keras import backend as K
 
-# Recall
-def recall(y_true, y_pred):
-    """Recall metric.
-
-    Only computes a batch-wise average of recall.
-
-    Computes the recall, a metric for multi-label classification of
-    how many relevant items are selected.
-    """
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    recall = true_positives / (possible_positives + K.epsilon())
-    return recall
-
-  # Precision
-def precision(y_true, y_pred):
-    """Precision metric.
-
-    Only computes a batch-wise average of precision.
-
-    Computes the precision, a metric for multi-label classification of
-    how many selected items are relevant.
-    """
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    return precision
-  
-
 def f1(y_true, y_pred):
-    """F1 metric.
+    y_pred = K.round(y_pred)
+    tp = K.sum(K.cast(y_true*y_pred, 'float'), axis=0)
+    # tn = K.sum(K.cast((1-y_true)*(1-y_pred), 'float'), axis=0)
+    fp = K.sum(K.cast((1-y_true)*y_pred, 'float'), axis=0)
+    fn = K.sum(K.cast(y_true*(1-y_pred), 'float'), axis=0)
 
-    Only computes a batch-wise average of recall.
+    p = tp / (tp + fp + K.epsilon())
+    r = tp / (tp + fn + K.epsilon())
 
-    Computes the recall, a metric for multi-label classification of
-    how many relevant items are selected.
-    """
-    pr = precision(y_true, y_pred)
-    rec = recall(y_true, y_pred)
-    return 2*((pr*rec)/(pr+rec+K.epsilon()))
+    f1 = 2*p*r / (p+r+K.epsilon())
+    f1 = tf.where(tf.is_nan(f1), tf.zeros_like(f1), f1)
+    return K.mean(f1)
