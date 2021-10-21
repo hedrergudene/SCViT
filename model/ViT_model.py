1e0d16d71c3264552cf7d1244910043b5f653b26
Antonio Zarauz Moreno <87022369+hedrergudene@users.noreply.github.com>
2021-10-21 09:49:25 +0200

    adapt final pooling in conv

--- model/ViT_model.py
+++ model/ViT_model.py
@@ -150,20 +150,9 @@ class HViT(tf.keras.layers.Layer):
                                             tf.keras.layers.GlobalAveragePooling1D(),
                                             ])
         elif self.resampling_type in ['conv']:
-            if self.img_size//self.patch_size[-1]>2:
-                self.MLP = tf.keras.Sequential([tf.keras.layers.LayerNormalization(epsilon=1e-6),
-                                                tf.keras.layers.GlobalAveragePooling1D(),
-                                                ])
-            else:
-                self.MLP = tf.keras.Sequential([tf.keras.layers.LayerNormalization(epsilon=1e-6),
-                                                tf.keras.layers.Lambda(lambda y: tf.transpose(unflatten(y, self.num_channels), perm=[0,4,2,3,1])),
-                                                tf.keras.layers.Conv2D(1, self.num_patches[-1]//2, strides = self.num_patches[-1]//2, padding = 'same'),
-                                                tf.keras.layers.Lambda(lambda y: tf.transpose(y, perm=[0,4,2,3,1])),
-                                                tf.keras.layers.Lambda(lambda y: tf.squeeze(tf.reshape(y, shape=[-1,1,self.num_patches[-1]**2//16]), axis = 1)),
-                                                tf.keras.layers.LayerNormalization(epsilon=1e-6),
-                                                tf.keras.layers.Dense(self.projection_dim[-1]),
-                                                tf.keras.layers.Dropout(self.drop_linear),
-                                                ])
+            self.MLP = tf.keras.Sequential([tf.keras.layers.LayerNormalization(epsilon=1e-6),
+                                            tf.keras.layers.GlobalAveragePooling1D(),
+                                            ])
         elif self.resampling_type in ['standard']:
             self.MLP = tf.keras.Sequential([tf.keras.layers.LayerNormalization(epsilon=1e-6),
                                             tf.keras.layers.Flatten(),
