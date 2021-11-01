3743f46f5d5b69d85a7c8c72ebe0e7d43c7e100f
Antonio Zarauz Moreno <hedrergudene@gmail.com>
2021-11-01 22:49:14 +0100

    Correct upsampling and downsampling in pytorch

--- model/pytorch/functions.py
+++ model/pytorch/functions.py
@@ -1,4 +1,6 @@
 import torch
+import numpy as np
+from typing import List
 
 # Functions
 ## Patch
@@ -11,7 +13,7 @@ def patch(X:torch.Tensor,
     assert h%patch_size==0, f"Patch size must divide images height"
     assert w%patch_size==0, f"Patch size must divide images width"
     patches_desc = X.unfold(2, patch_size, patch_size).unfold(3, patch_size, patch_size)
-    patch_list = torch.flatten(patches_desc, 2,3).permute(0,2,1,3,4)
+    patch_list = torch.flatten(patches_desc, 2, 3).permute(0,2,1,3,4)
     return patch_list
 
 ## Unflatten
@@ -34,7 +36,45 @@ def unpatch(x, num_channels):
     restored_images = torch.squeeze(restored_images, dim = 1)
     return restored_images
 
+## Downsampling
+def downsampling(encoded_patches, num_channels):
+    _, _, embeddings = encoded_patches.size()
+    ch, h, w = num_channels, int(np.sqrt(embeddings/num_channels)), int(np.sqrt(embeddings/num_channels))
+    original_image = unpatch(unflatten(encoded_patches, num_channels), num_channels)
+    new_patches = patch(original_image, patch_size = h//2)
+    new_patches_flattened = torch.flatten(new_patches, start_dim = -3, end_dim = -1)
+    return new_patches_flattened
+
+## Upsampling
+def upsampling(encoded_patches, num_channels):
+    _, _, embeddings = encoded_patches.size()
+    h, w, ch = int(np.sqrt(embeddings/num_channels)), int(np.sqrt(embeddings/num_channels)), num_channels
+    original_image = unpatch(unflatten(encoded_patches, num_channels), num_channels)
+    new_patches = patch(original_image, patch_size = h*2)
+    new_patches_flattened = torch.flatten(new_patches, start_dim = -3, end_dim = -1)
+    return new_patches_flattened
+
+
 # Layers
+
+## DoubleConv
+class DoubleConv(torch.nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, num_channels:int):
+        super(DoubleConv).__init__()
+        self.double_conv = torch.nn.Sequential(
+            torch.nn.Conv2d(num_channels, num_channels, kernel_size=3, padding='same'),
+            torch.nn.BatchNorm2d(num_channels),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Conv2d(num_channels, num_channels, kernel_size=3, padding='same'),
+            torch.nn.BatchNorm2d(num_channels),
+            torch.nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
 ## Patch Encoder
 class PatchEncoder(torch.nn.Module):
     def __init__(self,
@@ -186,3 +226,112 @@ class ReAttentionTransformerEncoder(torch.nn.Module):
         encoded_patches = self.FeedForward(encoded_patches) + encoded_patches
         encoded_patches = self.LN2(encoded_patches)
         return encoded_patches
+
+## Upsampling
+class Upsampling(torch.nn.Module):
+    def __init__(self,
+                 img_size:int,
+                 patch_size:List[int],
+                 num_channels:int,
+                 projection_dim:int=None,
+                 upsampling_type:str='conv',
+                 ):
+        super(Upsampling, self).__init__()
+        # Validation
+        assert upsampling_type in ['max', 'conv'], f"Upsampling type must either be 'max' or 'conv'."
+        assert patch_size[0]<patch_size[1], f"When upsampling, patch_size[0]<patch_size[1]."
+        # Parameters
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.num_patches = [(self.img_size//patch)**2 for patch in self.patch_size]
+        self.num_channels = num_channels
+        self.projection_dim = [projection_dim if projection_dim is not None else self.num_channels*patch**2 for patch in self.patch_size]
+        self.ratio = (max(self.patch_size)//min(self.patch_size))**2
+        self.kernel_size = int(np.sqrt(self.ratio))
+        self.final_proj_dim = self.projection_dim[0]//self.ratio
+        self.standard_proj_dim = self.projection_dim[0]*self.ratio
+        self.upsampling_type = upsampling_type
+        # Layers
+        self.proj = torch.nn.Linear(self.final_proj_dim, self.projection_dim[1])
+        if self.upsampling_type=='conv':
+            self.sequence = torch.nn.Sequential(
+                torch.nn.Conv2d(self.num_patches[0], self.num_patches[1], kernel_size = self.kernel_size, stride = self.kernel_size),
+                DoubleConv(self.num_patches[1]),
+            )
+
+    def forward(self, encoded_patches):
+        if self.upsampling_type=='conv':
+            encoded_patches = torch.transpose(unflatten(encoded_patches, self.num_channels), 2, 1)
+            encoded_patches = torch.transpose(torch.stack([self.sequence(y) for y in encoded_patches], dim=0), 2, 1)
+            encoded_patches = torch.flatten(encoded_patches, start_dim=2, end_dim=-1)
+            encoded_patches = self.proj(encoded_patches)
+            return encoded_patches
+        elif self.upsampling_type=='max':
+            encoded_patches = encoded_patches.permute((0,2,1))
+            encoded_patches = torch.reshape(encoded_patches, (-1, self.projection_dim[0], int(np.sqrt(self.num_patches[0])), int(np.sqrt(self.num_patches[0]))))
+            encoded_patches = torch.max_pool2d(encoded_patches, kernel_size = self.kernel_size, stride = self.kernel_size)
+            encoded_patches = torch.flatten(encoded_patches.permute((0,2,3,1)), start_dim=1, end_dim=2)
+            return encoded_patches
+
+## Downsampling
+class Downsampling(torch.nn.Module):
+    def __init__(self,
+                 img_size:int,
+                 patch_size:List[int],
+                 num_channels:int,
+                 projection_dim:int=None,
+                 downsampling_type:str='conv',
+                 ):
+        super(Downsampling, self).__init__()
+        # Validation
+        assert downsampling_type in ['bilinear', 'conv'], f"Upsampling type must either be 'bilinear' or 'conv'."
+        assert patch_size[0]>patch_size[1], f"When upsampling, patch_size[0]>patch_size[1]."
+        # Parameters
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.num_patches = [(self.img_size//patch)**2 for patch in self.patch_size]
+        self.num_channels = num_channels
+        self.projection_dim = [projection_dim if projection_dim is not None else self.num_channels*patch**2 for patch in self.patch_size]
+        self.ratio = (max(self.patch_size)//min(self.patch_size))**2
+        self.kernel_size = int(np.sqrt(self.ratio))
+        self.final_proj_dim = self.projection_dim[0]*self.ratio
+        self.standard_proj_dim = self.projection_dim[0]//self.ratio
+        self.downsampling_type = downsampling_type
+        # Layers
+        self.proj = torch.nn.Linear(self.final_proj_dim, self.projection_dim[1])
+        if self.downsampling_type=='conv':
+            self.sequence = torch.nn.Sequential(
+                torch.nn.ConvTranspose2d(self.num_patches[0], self.num_patches[1], kernel_size = self.kernel_size, stride = self.kernel_size),
+                DoubleConv(self.num_patches[1]),
+            )
+        if self.downsampling_type=='bilinear':
+            self.bil = torch.nn.UpsamplingBilinear2d(scale_factor=self.kernel_size)
+            self.sequence = torch.nn.Sequential(
+                DoubleConv(self.num_patches[1]),
+            )
+
+    def forward(self, encoded_patches):
+        if self.downsampling_type=='conv':
+            encoded_patches = torch.transpose(unflatten(encoded_patches, self.num_channels), 2, 1)
+            encoded_patches = torch.transpose(torch.stack([self.sequence(y) for y in encoded_patches], dim=0), 2, 1)
+            encoded_patches = torch.flatten(encoded_patches, start_dim=2, end_dim=-1)
+            encoded_patches = self.proj(encoded_patches)
+            return encoded_patches
+        elif self.downsampling_type=='bilinear':
+            # Unflatten
+            encoded_patches = unflatten(encoded_patches, self.num_channels)
+            ps = encoded_patches.shape[-1]
+            # Bilinear upsampling
+            encoded_patches = torch.stack([self.bil(y) for y in encoded_patches], dim=0)
+            encoded_patches = encoded_patches.permute((0,1,3,4,2))
+            # Create subpatches
+            encoded_patches = encoded_patches.unfold(-3, ps, ps).unfold(-3, ps, ps)
+            # Flat to encode
+            encoded_patches = torch.flatten(encoded_patches, start_dim=1, end_dim=3)
+            # Permute channels and patches to apply double conv
+            encoded_patches = encoded_patches.permute((0,2,1,3,4))
+            encoded_patches = torch.stack([self.sequence(y) for y in encoded_patches], dim=0)
+            encoded_patches = encoded_patches.permute((0,2,1,3,4))
+            # Final flat of patches
+            encoded_patches = torch.flatten(encoded_patches, start_dim=-3, end_dim=-1)
+            return encoded_patches
