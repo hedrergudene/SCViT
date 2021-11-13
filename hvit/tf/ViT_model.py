0fcbd9e7474c36fad127b8e3248720238eb00014
Antonio Zarauz Moreno <hedrergudene@gmail.com>
2021-11-13 10:33:12 +0100

    change ViT

--- hvit/tf/ViT_model.py
+++ hvit/tf/ViT_model.py
@@ -207,123 +207,93 @@ class HViT(tf.keras.layers.Layer):
         return logits
 
 # HViT for benchmark
-#class HViT(tf.keras.layers.Layer):
-#    def __init__(self,
-#                 img_size:int=128,
-#                 patch_size:List[int]=8,
-#                 projection_dim:int=768,
-#                 num_channels:int=3,
-#                 num_heads:int=8,
-#                 depth:int=4,
-#                 mlp_head_units:List[int]=[256,128],
-#                 num_classes:int=100,
-#                 hidden_unit_factor:float=2.,
-#                 drop_attn:float=.2,
-#                 drop_proj:float=.2,
-#                 drop_linear:float=.2,
-#                 resampling_type:str='conv',
-#                 original_attn:bool=True,
-#                 ):
-#        super(HViT, self).__init__()
-#        #Validations
-#        assert resampling_type in ['max', 'conv'], f"Resampling type must be either 'max' or 'conv'."
-#        # Parameters
-#        self.img_size = img_size
-#        self.patch_size = patch_size
-#        self.projection_dim = projection_dim
-#        self.num_channels = num_channels
-#        self.num_heads = num_heads
-#        self.depth = depth
-#        self.mlp_head_units = [self.projection_dim] + mlp_head_units + [num_classes]
-#        self.drop_attn = drop_attn
-#        self.drop_proj = drop_proj
-#        self.drop_linear = drop_linear
-#        self.resampling_type = resampling_type
-#        self.original_attn = original_attn
-#        self.hidden_units = int(hidden_unit_factor*self.projection_dim)
-#        ## Parameters computations
-#        self.patch_size_list = [self.patch_size*(2**i) for i in range(int(np.sqrt(self.img_size//self.patch_size))+1)]
-#        self.num_patches = [(self.img_size//ps)**2 for ps in self.patch_size_list]
-#
-#        # Layers
-#        ##Positional Encoding
-#        self.PE = PatchEncoder(self.img_size, self.patch_size, self.num_channels, self.projection_dim)
-#        ##Encoder
-#        self.Encoder = []
-#        self.Encoder_RS = []        
-#        if self.original_attn:
-#            for i in range(len(self.patch_size_list)-1):
-#                self.Encoder.append(    
-#                                    AttentionTransformerEncoder(self.img_size,
-#                                                                self.patch_size_list[i],
-#                                                                self.num_channels,
-#                                                                self.num_heads,
-#                                                                self.depth,
-#                                                                self.projection_dim, 
-#                                                                self.hidden_units,
-#                                                                self.drop_attn,
-#                                                                self.drop_proj,
-#                                                                )
-#                                        )
-#                self.Encoder_RS.append(Resampling(self.img_size, self.patch_size_list[i:i+2], self.num_channels, self.projection_dim, self.resampling_type))
-#        else:
-#            for i in range(len(self.patch_size)):
-#                self.Encoder.append(
-#                                    ReAttentionTransformerEncoder(self.img_size,
-#                                                                  self.patch_size_list[i],
-#                                                                  self.num_channels,
-#                                                                  self.num_heads,
-#                                                                  self.depth,
-#                                                                  self.projection_dim,
-#                                                                  self.hidden_units,
-#                                                                  self.drop_attn,
-#                                                                  self.drop_proj,
-#                                                                  )
-#                                        )
-#                self.Encoder_RS.append(Resampling(self.img_size, self.patch_size[i:i+2], self.num_channels, self.projection_dim, self.resampling_type))
-#        ##MLP
-#        for j in range(len(self.mlp_head_units)-1):
-#            self.MLP.add(tf.keras.layers.Dense(i))
-#            if (j+2)<len(self.mlp_head_units):
-#                self.MLP.add(tf.keras.layers.Dropout(self.drop_linear))
-#
-#
-#    def get_config(self):
-#
-#        config = super(HViT, self).get_config().copy()
-#        config.update({
-#                        'img_size':self.img_size,
-#                        'patch_size':self.patch_size,
-#                        'patch_size_rev':self.patch_size_rev,
-#                        'num_channels':self.num_channels,
-#                        'num_heads':self.num_heads,
-#                        'transformer_layers':self.transformer_layers,
-#                        'mlp_head_units':self.mlp_head_units,
-#                        'num_classes':self.num_classes,
-#                        'drop_attn':self.drop_attn,
-#                        'drop_proj':self.drop_proj,
-#                        'drop_linear':self.drop_linear,
-#                        'resampling_type':self.resampling_type,
-#                        'original_attn':self.original_attn,
-#                        'num_patches':self.num_patches,
-#                        'projection_dim':self.projection_dim,
-#                        'hidden_units':self.hidden_units,
-#                        'PE':self.PE,
-#                        'Encoder':self.Encoder,
-#                        'Encoder_RS':self.Encoder_RS,
-#                        'MLP':self.MLP,
-#                        'bias_initializer':self.bias_initializer,
-#                        })
-#        return config
-#
-#    
-#    def call(self, X:tf.Tensor):
-#        # Patch
-#        encoded = self.PE(X)
-#        # Encoder
-#        for i in range(len(self.patch_size_list)):
-#            encoded = self.Encoder[i](encoded)
-#            encoded = self.Encoder_RS[i](encoded)
-#        # MLP
-#        logits = self.MLP(encoded)
-#        return logits
+class HViT_benchmark(tf.keras.layers.Layer):
+    def __init__(self,
+                 img_size:int=128,
+                 patch_size:List[int]=8,
+                 projection_dim:int=768,
+                 num_channels:int=3,
+                 num_heads:int=8,
+                 depth:int=4,
+                 mlp_head_units:List[int]=[256,128],
+                 num_classes:int=100,
+                 hidden_unit_factor:float=2.,
+                 drop_attn:float=.2,
+                 drop_proj:float=.2,
+                 drop_linear:float=.2,
+                 resampling_type:str='conv',
+                 original_attn:bool=True,
+                 ):
+        super(HViT, self).__init__()
+        #Validations
+        assert resampling_type in ['max', 'conv'], f"Resampling type must be either 'max' or 'conv'."
+        # Parameters
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.projection_dim = projection_dim
+        self.num_channels = num_channels
+        self.num_heads = num_heads
+        self.depth = depth
+        self.mlp_head_units = [self.projection_dim] + mlp_head_units + [num_classes]
+        self.drop_attn = drop_attn
+        self.drop_proj = drop_proj
+        self.drop_linear = drop_linear
+        self.resampling_type = resampling_type
+        self.original_attn = original_attn
+        self.hidden_units = int(hidden_unit_factor*self.projection_dim)
+        ## Parameters computations
+        self.patch_size_list = [self.patch_size*(2**i) for i in range(int(np.sqrt(self.img_size//self.patch_size))+1)]
+        self.num_patches = [(self.img_size//ps)**2 for ps in self.patch_size_list]
+
+        # Layers
+        ##Positional Encoding
+        self.PE = PatchEncoder(self.img_size, self.patch_size, self.num_channels, self.projection_dim)
+        ##Encoder
+        self.Encoder = []
+        self.Encoder_RS = []        
+        if self.original_attn:
+            for i in range(len(self.patch_size_list)-1):
+                self.Encoder.append(    
+                                    AttentionTransformerEncoder(self.img_size,
+                                                                self.patch_size_list[i],
+                                                                self.num_channels,
+                                                                self.num_heads,
+                                                                self.depth,
+                                                                self.projection_dim, 
+                                                                self.hidden_units,
+                                                                self.drop_attn,
+                                                                self.drop_proj,
+                                                                )
+                                        )
+                self.Encoder_RS.append(Resampling(self.img_size, self.patch_size_list[i:i+2], self.num_channels, self.projection_dim, self.resampling_type))
+        else:
+            for i in range(len(self.patch_size)):
+                self.Encoder.append(
+                                    ReAttentionTransformerEncoder(self.img_size,
+                                                                  self.patch_size_list[i],
+                                                                  self.num_channels,
+                                                                  self.num_heads,
+                                                                  self.depth,
+                                                                  self.projection_dim,
+                                                                  self.hidden_units,
+                                                                  self.drop_attn,
+                                                                  self.drop_proj,
+                                                                  )
+                                        )
+                self.Encoder_RS.append(Resampling(self.img_size, self.patch_size[i:i+2], self.num_channels, self.projection_dim, self.resampling_type))
+        ##MLP
+        for j in range(len(self.mlp_head_units)-1):
+            self.MLP.add(tf.keras.layers.Dense(i))
+            if (j+2)<len(self.mlp_head_units):
+                self.MLP.add(tf.keras.layers.Dropout(self.drop_linear))
+  
+    def call(self, X:tf.Tensor):
+        # Patch
+        encoded = self.PE(X)
+        # Encoder
+        for i in range(len(self.patch_size_list)):
+            encoded = self.Encoder[i](encoded)
+            encoded = self.Encoder_RS[i](encoded)
+        # MLP
+        logits = self.MLP(encoded)
+        return logits
